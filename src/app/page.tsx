98eabaf59dd8068504f2c0b25da94d95642483eb
Adle25 <askaradilet41@gmail.com>
2024-08-25 23:11:51 +0500

    Added authentication screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@/components/ui/button";
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export default function Home() {
   return (
-    <Button variant="destructive">Click me!</Button>
+    <AuthScreen />
   );
-}
+};
