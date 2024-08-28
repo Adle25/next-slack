90d3f522caba2967e7614465d75e28b3d570aa67
Adle25 <askaradilet41@gmail.com>
2024-08-28 23:48:57 +0500

    Added members functionality

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -2,10 +2,39 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { auth } from "./auth";
 
+const generateCode = () => {
+    const code = Array.from(
+        { length: 6 },
+        () => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
+    ).join("");
+
+    return code;
+};
+
 export const get = query({
     args: {},
     handler: async (ctx) => {
-        return await ctx.db.query("workspaces").collect();
+        const userId = await auth.getUserId(ctx);
+
+        if (!userId) {
+            return [];
+        }
+
+        const members = await ctx.db.query("members").withIndex("by_user_id", (q) => q.eq("userId", userId)).collect();
+
+        const workspaceIds = members.map((member) => member.workspaceId);
+
+        const workspaces = [];
+
+        for (const workspaceId of workspaceIds) {
+            const workspace = await ctx.db.get(workspaceId);
+
+            if (workspace) {
+                workspaces.push(workspace);
+            }
+        }
+
+        return workspaces;
     },
 });
 
@@ -28,6 +57,12 @@ export const create = mutation({
             joinCode: joinCode
         });
 
+        await ctx.db.insert("members", {
+            userId,
+            workspaceId,
+            role: "admin"
+        });
+
 
         return workspaceId;
     }
@@ -42,6 +77,12 @@ export const getById = query({
 
         if (!userId) {
             throw new Error("Unauthorized");
+        }
+
+        const member = await ctx.db.query("members").withIndex("by_workspace_id_user_id", (q) => q.eq("workspaceId", args.id).eq("userId", userId)).unique();
+
+        if (!member) {
+            return null;
         }
 
         return await ctx.db.get(args.id);
--- src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -18,7 +18,7 @@ export const WorkspaceSwitcher = () => {
 
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger>
+            <DropdownMenuTrigger asChild>
                 <Button className="size-9 relative overflow-hidden bg-[#ABABAB] hover:bg-[#ABABAB]/80 text-slate-800 font-semibold text-xl">
                     {workspaceLoading ? (
                         <Loader className="size-5 animate-spin shrink-0" />
