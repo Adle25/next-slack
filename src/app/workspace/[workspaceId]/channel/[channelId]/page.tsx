3ec385038261f22e45d8025b190c38b54cbff477
Adle25 <askaradilet41@gmail.com>
2024-09-01 14:49:13 +0500

    Started channel page

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,4 +1,31 @@
+"use client";
+
+import { useGetChannel } from "@/features/channels/api/use-get-channel";
+import { useChannelId } from "@/hooks/use-channel-id";
+import { Loader, TriangleAlert } from "lucide-react";
+
 const ChannelIdPage = () => {
+    const channelId = useChannelId();
+
+    const { data: channel, isLoading: channelLoading } = useGetChannel({ id: channelId });
+
+    if (channelLoading) {
+        return (
+            <div className="h-full flex-1 flex items-center justify-center">
+                <Loader className="size-5 animate-spin text-muted-foreground" />
+            </div>
+        );
+    }
+
+    if (!channel) {
+        return (
+            <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center">
+                <TriangleAlert className="size-5 text-muted-foreground" />
+                <span className="text-sm text-muted-foreground">Channel not found</span>
+            </div>
+        );
+    }
+
     return (
         <div>Channel ID Page</div>
     );
