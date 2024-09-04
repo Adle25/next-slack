4bbc1ed070b77781876d26448366dbc3b5e07e36
Adle25 <askaradilet41@gmail.com>
2024-09-04 22:48:55 +0500

    Added image picker

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -3,12 +3,13 @@ import "quill/dist/quill.snow.css";
 import { MutableRefObject, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { Button } from "./ui/button";
 import { PiTextAa } from "react-icons/pi";
-import { ImageIcon, Smile } from "lucide-react";
+import { ImageIcon, Smile, XIcon } from "lucide-react";
 import { MdSend } from "react-icons/md";
 import { Hint } from "./hint";
 import { Delta, Op } from "quill/core";
 import { cn } from "@/lib/utils";
 import { EmojiPopover } from "./emoji-popover";
+import Image from "next/image";
 
 type EditorValue = {
     image: File | null;
@@ -27,6 +28,7 @@ interface EditorProps {
 
 const Editor = ({ variant = "create", onSubmit, onCancel, placeholder = "Write something...", defaultValue = [], disabled = false, innerRef }: EditorProps) => {
     const [text, setText] = useState("");
+    const [image, setImage] = useState<File | null>(null);
     const [isToolbarVisible, setIsToolbarVisible] = useState(true);
 
     const containerRef = useRef<HTMLDivElement>(null);
@@ -35,6 +37,7 @@ const Editor = ({ variant = "create", onSubmit, onCancel, placeholder = "Write s
     const quillRef = useRef<Quill | null>(null);
     const defaultValueRef = useRef(defaultValue);
     const disabledRef = useRef(disabled);
+    const imageElementRef = useRef<HTMLInputElement>(null);
 
     useLayoutEffect(() => {
         submitRef.current = onSubmit;
@@ -130,8 +133,27 @@ const Editor = ({ variant = "create", onSubmit, onCancel, placeholder = "Write s
 
     return (
         <div className="flex flex-col">
+            <input type="file" accept="image/*" ref={imageElementRef} onChange={(event) => setImage(event.target.files![0])} className="hidden" />
             <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
                 <div ref={containerRef} className="h-full ql-custom" />
+                {!!image && (
+                    <div className="p-2">
+                        <div className="relative size-[62px] flex items-center justify-center group/image">
+                            <Hint label="remove image">
+                                <button
+                                    onClick={() => {
+                                        setImage(null);
+                                        imageElementRef.current!.value = "";
+                                    }}
+                                    className="hidden group-hover/image:flex rounded-full bg-black/70 hover:bg-black absolute -top-2.5 -right-2.5 text-white size-6 z-[4] border-2 border-white items-center justify-center"
+                                >
+                                    <XIcon className="size-3.5" />
+                                </button>
+                            </Hint>
+                            <Image src={URL.createObjectURL(image)} alt="Uploaded" fill className="rounded-xl overflow-hidden border object-cover" />
+                        </div>
+                    </div>
+                )}
                 <div className="flex px-2 pb-2 z-[5]">
                     <Hint label={isToolbarVisible ? "Hide formatting" : "Show formatting"}>
                         <Button disabled={disabled} size="iconSm" variant="ghost" onClick={toggleToolbar}>
@@ -145,7 +167,7 @@ const Editor = ({ variant = "create", onSubmit, onCancel, placeholder = "Write s
                     </EmojiPopover>
                     {variant === "create" && (
                         <Hint label="Image">
-                            <Button disabled={disabled} size="iconSm" variant="ghost" onClick={() => { }}>
+                            <Button disabled={disabled} size="iconSm" variant="ghost" onClick={() => imageElementRef.current?.click()}>
                                 <ImageIcon className="size-4" />
                             </Button>
                         </Hint>
