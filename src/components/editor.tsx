c385aed087b51d5ce2a4a89cff8bfe7b70c696e8
Adle25 <askaradilet41@gmail.com>
2024-09-04 22:35:15 +0500

    Added emoji to editor

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -8,6 +8,7 @@ import { MdSend } from "react-icons/md";
 import { Hint } from "./hint";
 import { Delta, Op } from "quill/core";
 import { cn } from "@/lib/utils";
+import { EmojiPopover } from "./emoji-popover";
 
 type EditorValue = {
     image: File | null;
@@ -119,6 +120,12 @@ const Editor = ({ variant = "create", onSubmit, onCancel, placeholder = "Write s
         }
     };
 
+    const onEmojiSelect = (emoji: any) => {
+        const quill = quillRef.current;
+
+        quill?.insertText(quill?.getSelection()?.index || 0, emoji.native);
+    };
+
     const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
     return (
@@ -131,11 +138,11 @@ const Editor = ({ variant = "create", onSubmit, onCancel, placeholder = "Write s
                             <PiTextAa className="size-4" />
                         </Button>
                     </Hint>
-                    <Hint label="Emoji">
-                        <Button disabled={disabled} size="iconSm" variant="ghost" onClick={() => { }}>
+                    <EmojiPopover onEmojiSelect={onEmojiSelect}>
+                        <Button disabled={disabled} size="iconSm" variant="ghost">
                             <Smile className="size-4" />
                         </Button>
-                    </Hint>
+                    </EmojiPopover>
                     {variant === "create" && (
                         <Hint label="Image">
                             <Button disabled={disabled} size="iconSm" variant="ghost" onClick={() => { }}>
@@ -160,9 +167,15 @@ const Editor = ({ variant = "create", onSubmit, onCancel, placeholder = "Write s
                     )}
                 </div>
             </div>
-            <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
-                <p><strong>Shift + Return</strong> to add a new line</p>
-            </div>
+            {variant === "create" && (
+                <div className={cn(
+                    "p-2 text-[10px] text-muted-foreground flex justify-end opacity-0 transition",
+                    !isEmpty && "opacity-100"
+                )}>
+                    <p><strong>Shift + Return</strong> to add a new line</p>
+                </div>
+            )}
+
         </div>
     );
 };
