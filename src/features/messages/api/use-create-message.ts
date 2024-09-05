043d0a2356cecdd94f018296c8afc250bae84bcc
Adle25 <askaradilet41@gmail.com>
2024-09-05 22:02:44 +0500

    Finalized schema

--- src/features/messages/api/use-create-message.ts
+++ src/features/messages/api/use-create-message.ts
@@ -9,6 +9,7 @@ type RequestType = {
     workspaceId: Id<"workspaces">;
     channelId?: Id<"channels">;
     parentMessageId?: Id<"messages">;
+    conversationId?: Id<"conversations">;
 };
 
 type ResponseType = Id<"messages"> | null;
